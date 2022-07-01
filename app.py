4da84b357326015f712637c0488ed217938c014d
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-07-01 13:05:21 -0400

    flask-wtforms implementation, csrv token

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
 from flask import Flask
 from flask import render_template, request, redirect
 
-from sqlalchemy import create_engine, select
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 
-from config import host, port, database, user, password
-from classes import build_table
+from config import host, port, database, user, password, secret_key
+from classes import build_table, IndexPageForm
 
 conn_str = f"postgresql+psycopg2://{user}:{password}@{host}/{database}"
 engine = create_engine(conn_str, echo=True, future=True)
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = secret_key
 
 # Get info from user to pass into underwriting db
 @app.route("/", methods=["GET", "POST"])
 def index():
 
     if request.method == "GET":
-        # Populate height list from build table for drop-down menu
-        height_list = []
-        with Session(engine) as session:
-            for row in session.execute(select(build_table.c.height)):
-                height_list.append(row)
-        return render_template("index.html", height_list=height_list)
+        form = IndexPageForm()
+        return render_template("index.html", form=form)
     
     # if request is "POST" (meaning we have submitted the form)
-    # save the information into a db Table called 'cases' and redirect to /health_profile
+    # validate and save the information into a db Table called 'cases' and redirect to /health_profile
 
 @app.route("/health_profile", methods=["POST"])
 def health_profile():
--- classes.py
+++ classes.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table, Column, Integer, String, Boolean
-from sqlalchemy import insert, text
+from sqlalchemy import insert, text, select
+
+from sqlalchemy.orm import Session
+
+from flask_wtf import FlaskForm
+from wtforms import DateField, SelectField, IntegerField, SubmitField, validators
 from config import host, port, database, user, password
 
 conn_str = f"postgresql+psycopg2://{user}:{password}@{host}/{database}"
@@ -10,3 +15,22 @@ metadata_obj = MetaData()
 
 # create Table object via reflection from DB
 build_table = Table("build", metadata_obj, autoload_with=engine)
+
+height_list = ["Please select..."]
+with Session(engine) as session:
+    for row in session.execute(select(build_table.c.height)):
+        height_list.append(row[0])
+
+# Form classes
+class IndexPageForm(FlaskForm):
+    birthdate = DateField('Birthdate:', [validators.InputRequired()], format="%m/%d/%Y")
+    gender = SelectField('Gender:', [validators.InputRequired()], choices=["Please select...", "Male", "Female"])
+    height = SelectField('Height (in feet and inches):', [validators.InputRequired()], choices=height_list)
+    weight = IntegerField('Weight (in pounds):', [validators.InputRequired(), validators.Length(min=1,max=3)])
+    nicotine = SelectField('Nicotine use (Last 12 months):', [validators.InputRequired()], choices=[
+        "Please select...", 
+        "Yes", 
+        "No", 
+        "Other (vape, chewing tobacco, cigars, etc)"
+        ])
+    submit = SubmitField("Continue")
