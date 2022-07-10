ec3dea1625e30aadad82015d3d6ed44fdf9da9fb
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-07-09 23:01:30 -0400

    renamed function for clarity

--- static/health_hx.js
+++ static/health_hx.js
@@ -6,7 +6,7 @@ let diabetes_q = document.getElementById("diabetes")
 let insulin_q = document.getElementById("insulin")
 
 // Event handler function to toggle hidden status of each duration select field
-function switch_hidden(event, label_id, field_id) {
+function toggle_hidden(event, label_id, field_id) {
     let label = document.getElementById(label_id)
     let field = document.getElementById(field_id)
     if ((label.hidden == true) && (field.hidden == true)) {
@@ -20,10 +20,10 @@ function switch_hidden(event, label_id, field_id) {
 }
 
 // Event listeners
-heart_attack_q.addEventListener("click", (event) => switch_hidden(event, "heart_attack_dur_lbl", "heart_attack_dur"))
-stroke_q.addEventListener("click", (event) => switch_hidden(event, "stroke_dur_lbl", "stroke_dur"))
-tia_q.addEventListener("click", (event) => switch_hidden(event, "tia_dur_lbl", "tia_dur"))
-cardio_surgery_q.addEventListener("click", (event) => switch_hidden(event, "cardio_surgery_dur_lbl", "cardio_surgery_dur"))
-diabetes_q.addEventListener("click", (event) => switch_hidden(event, "insulin_lbl", "insulin"))
-diabetes_q.addEventListener("click", (event) => switch_hidden(event, "complications_lbl", "complications"))
-insulin_q.addEventListener("click", (event) => switch_hidden(event, "insulin_dur_lbl", "insulin_dur"))
+heart_attack_q.addEventListener("click", (event) => toggle_hidden(event, "heart_attack_dur_lbl", "heart_attack_dur"))
+stroke_q.addEventListener("click", (event) => toggle_hidden(event, "stroke_dur_lbl", "stroke_dur"))
+tia_q.addEventListener("click", (event) => toggle_hidden(event, "tia_dur_lbl", "tia_dur"))
+cardio_surgery_q.addEventListener("click", (event) => toggle_hidden(event, "cardio_surgery_dur_lbl", "cardio_surgery_dur"))
+diabetes_q.addEventListener("click", (event) => toggle_hidden(event, "insulin_lbl", "insulin"))
+diabetes_q.addEventListener("click", (event) => toggle_hidden(event, "complications_lbl", "complications"))
+insulin_q.addEventListener("click", (event) => toggle_hidden(event, "insulin_dur_lbl", "insulin_dur"))
