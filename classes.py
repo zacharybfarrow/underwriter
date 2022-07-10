da5124423c0c00a3537a2e252aa13cc7e5050d96
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-07-10 10:07:50 -0400

    added cancer questions to health_hx

--- classes.py
+++ classes.py
@@ -23,7 +23,37 @@ with Session(engine) as session:
     for row in session.execute(select(build_table.c.height)):
         height_list.append(row[0])
 
-dur_list = ["Less than 1 year ago", "1 - 2 years ago", "2 - 3 years ago", "3+ years ago"]
+dur_list = [
+    "Select from list...",
+    "Less than 1 year ago", 
+    "1 - 2 years ago", 
+    "2 - 3 years ago", 
+    "3 - 4 years ago", 
+    "4 - 5 years ago", 
+    "5 - 6 years ago", 
+    "6 - 7 years ago",
+    "7 - 8 years ago", 
+    "8 - 9 years ago", 
+    "Over 10 years ago"
+]
+
+cancer_list = [
+    "Select from list...",
+    "Breast",
+    "Lung",
+    "Prostate",
+    "Colon",
+    "Melanoma",
+    "Bladder",
+    "Non-Hodgkin's Lymphoma",
+    "Kidney",
+    "Endometrial",
+    "Leukemia",
+    "Pancreatic",
+    "Thyroid",
+    "Liver",
+    "Other"
+]
 
 # Form classes
 class IndexPageForm(FlaskForm):
@@ -53,4 +83,9 @@ class HealthHxForm(FlaskForm):
     insulin = BooleanField('Insulin use? ')
     complications = BooleanField('Complications of diabetes? ')
     insulin_dur = SelectField('Last used insulin? ', choices=dur_list)
+    cancer = BooleanField('Cancer (other than basal cell or squamous cell)? ')
+    cancer_type = SelectField('Type of cancer? ', choices=cancer_list)
+    cancer_dur = SelectField('Time since last treatment? ', choices=dur_list)
+    cancer_multi = BooleanField('Multiple occurences of cancer? ')
+    cancer_recur = BooleanField('Recurrance of cancer? ')
     submit = SubmitField("Continue")
--- static/health_hx.js
+++ static/health_hx.js
@@ -4,6 +4,7 @@ let tia_q = document.getElementById("tia")
 let cardio_surgery_q = document.getElementById("cardio_surgery")
 let diabetes_q = document.getElementById("diabetes")
 let insulin_q = document.getElementById("insulin")
+let cancer_q = document.getElementById("cancer")
 
 // Event handler function to toggle hidden status of each duration select field
 function toggle_hidden(event, label_id, field_id) {
@@ -19,6 +20,16 @@ function toggle_hidden(event, label_id, field_id) {
     }
 }
 
+function toggle_div_hidden(event, div_id) {
+    let div = document.getElementById(div_id)
+    if (div.hidden == true) {
+        div.hidden = false
+    }
+    else {
+        div.hidden = true
+    }
+}
+
 // Event listeners
 heart_attack_q.addEventListener("click", (event) => toggle_hidden(event, "heart_attack_dur_lbl", "heart_attack_dur"))
 stroke_q.addEventListener("click", (event) => toggle_hidden(event, "stroke_dur_lbl", "stroke_dur"))
@@ -27,3 +38,18 @@ cardio_surgery_q.addEventListener("click", (event) => toggle_hidden(event, "card
 diabetes_q.addEventListener("click", (event) => toggle_hidden(event, "insulin_lbl", "insulin"))
 diabetes_q.addEventListener("click", (event) => toggle_hidden(event, "complications_lbl", "complications"))
 insulin_q.addEventListener("click", (event) => toggle_hidden(event, "insulin_dur_lbl", "insulin_dur"))
+cancer_q.addEventListener("click", (event) => toggle_div_hidden(event, "cancer_more"))
+// Alternate implementation to toggle hidden attribute of cancer_more div, using lambda function, 
+// opted for named func above for potential reusability
+// cancer_q.addEventListener(
+//    "click",
+//    function() {
+//        let div = document.getElementById("cancer_more")
+//        if (div.hidden == true) {
+//            div.hidden = false
+//        }
+//        else {
+//            div.hidden = true
+//       }
+//    }
+//)
