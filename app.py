a15df2515c13136f59144b9d472971dd935f1e8b
zacharybfarrow <zachary.b.farrow@gmail.com>
2022-07-09 22:43:52 -0400

    health_hx route logic

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 
 from config import host, port, database, user, password, secret_key
-from classes import IndexPageForm
+from classes import HealthHxForm, IndexPageForm
 from helpers import store_case_data
 
 conn_str = f"postgresql+psycopg2://{user}:{password}@{host}/{database}"
@@ -17,20 +17,47 @@ app.config['SECRET_KEY'] = secret_key
 @app.route("/", methods=["GET", "POST"])
 def index():
     form = IndexPageForm()
-    if form.validate_on_submit():
-        print("validated")
-        # Store user input in db
-        case = store_case_data(form)
-        # Send case data along with redirect
-        return redirect(url_for("health_hx", case=case))
+    # If form is being submitted
+    if request.method == "POST":
+        if form.validate_on_submit():
+            print("validated")
+            # Store user input in db
+            case = store_case_data(form)
+            # Send case data along with redirect
+            return redirect(url_for("health_hx", case=case))
 
-    # Institute some sort of error flashing
-    else:
-        print("did not validate")
-        print(form.birthdate)
-        print(form.errors)
+        # If form wasn't validated, handle errors
+        elif not form.validate_on_submit():
+            print(form.errors)
+            return render_template("index.html", form=form)
+        
+        # Accessing form from GET request
+    if request.method == "GET":
         return render_template("index.html", form=form)
 
 @app.route("/health_hx", methods=["GET", "POST"])
 def health_hx():
-    return render_template("health_hx.html", case=request.args.get("case"))
+    form = HealthHxForm()
+
+    # If form is being submitted
+    if request.method == "POST":
+        # Check for form validation
+        if form.validate_on_submit():
+            print("health_hx validated")
+            return redirect(url_for("rx"))
+            # Store user input in db
+        
+        return render_template("health_hx.html", form=form)
+
+    # If accessing from redirect
+    if request.method == "GET":
+        return render_template("health_hx.html", form=form)
+
+@app.route("/rx", methods=["GET", "POST"])
+def rx():
+    if request.method == "POST":
+        # submit the medications
+        return render_template("rx.html")
+    
+    if request.method == "GET":
+        return render_template("rx.html")
